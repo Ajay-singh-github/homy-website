96cb1bc9e120057699449ecbae45995f37bfd973
Vats Shivam <vatsshivam@Vatss-MacBook-Air.local>
2024-11-10 22:04:20 +0530

    .

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card"
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
+import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel"
 import { Star } from "lucide-react"
 import customer from "../assets/customer.png"
 
